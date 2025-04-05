e4d0868183e2564b293e018457b887067128ba71
giovanna-britto <giovanna.vieira@sou.inteli.edu.br>
2025-04-05 17:15:20 -0300

    feat: atualiza biblioteca para converter pdf

--- contrato_ai/app/utils/pdf.py
+++ contrato_ai/app/utils/pdf.py
@@ -1,29 +1,26 @@
+import os
 import pdfkit
 import tempfile
-import os
 
 def gerar_pdf_from_html_bytes(html: str) -> bytes:
     try:
-        # Caminho completo e correto
-        path_wkhtmltopdf = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
+        # Caminho absoluto correto do wkhtmltopdf.exe
+        path_wkhtmltopdf = os.path.abspath("wkhtmltopdf/bin/wkhtmltopdf.exe")
         config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
 
-        # Criar HTML temporário para inspeção
         temp_html = tempfile.NamedTemporaryFile(delete=False, suffix=".html", mode="w", encoding="utf-8")
         temp_html.write(html)
         temp_html.close()
         print("📄 HTML TEMP:", temp_html.name)
 
-        # Tenta gerar PDF
         pdf_bytes = pdfkit.from_file(temp_html.name, False, configuration=config)
         print("✅ PDF gerado com sucesso!")
 
-        # Remove arquivo temporário se quiser
         os.remove(temp_html.name)
         return pdf_bytes
 
     except Exception as e:
-        print("❌ ERRO COMPLETO:")
         import traceback
+        print("❌ ERRO COMPLETO:")
         traceback.print_exc()
         raise Exception("Falha ao gerar o PDF.")
